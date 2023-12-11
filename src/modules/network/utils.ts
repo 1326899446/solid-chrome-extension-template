d38603d157cb46cfcaee972d1e731a2a5aafbd92
022297 <yangwenxin@htsc.com>
2023-12-11 14:22:10 +0800

    feat: 完善补充原生参数的逻辑

--- src/modules/network/utils.ts
+++ src/modules/network/utils.ts
@@ -1,11 +1,11 @@
-import { globalState } from '../data/global';
-import { MapActionToServer, NATIVE_PARAMS_LIST } from './constant';
+import { globalState } from "../data/global";
+import { MapActionToServer, NATIVE_PARAMS_LIST } from "./constant";
 
 export function parseData(data) {
   // 将 a=1&b=2形式字符串解析为一个object
-  if(!data){
-    return {}
-  } 
+  if (!data) {
+    return {};
+  }
   const dataArr = data.split("&");
   const dataObj = dataArr.reduce((pre, cur) => {
     const singleDataArr = cur.split("=");
@@ -16,37 +16,61 @@ export function parseData(data) {
   }, {});
   return dataObj;
 }
-export function replaceNativeParams(data){
+export function replaceNativeParams(data) {
   const res = { ...data };
-  Object.keys(res).forEach((key)=>{
+  Object.keys(res).forEach((key) => {
     const value = decodeURIComponent(data[key]);
-    if(value[0]==="(" && value[value.length-1]===")" && value[1]==="$"){
-      const str = value.slice(2,value.length-1)
-      res[key] = globalState.appParams[globalState.app][str] || '';
+    if (
+      value[0] === "(" &&
+      value[value.length - 1] === ")" &&
+      value[1] === "$"
+    ) {
+      const str = value.slice(2, value.length - 1);
+      res[key] = globalState.appParams[globalState.app].locals[str] || "";
+      // 什么时候用变量的值，什么时候用用空
+      if (!globalState.loginStatus) {
+        if (["account", "jyloginflag"].includes(str.toLocaleLowerCase())) {
+          res[key] = "";
+        }
+      }
+      if (!globalState.weakLoginStatus) {
+        if (
+          [
+            "weakaccount",
+            "weaktoken",
+            "weakmobilecode",
+            "weakname",
+            "weakiconid",
+          ].includes(str.toLocaleLowerCase())
+        ) {
+          res[key] = "";
+        }
+      }
     }
-  })
-  return res
+  });
+  console.log("res", res, globalState);
+
+  return res;
 }
 export function sendRequestAgain({ method, url, headers, data }) {
   // 将收到的请求二次转发出去
   //http://221.6.6.237:18100
   const queryParams = getQueryParams(url);
-  const targetAddress = MapActionToServer(queryParams.action || '');
+  const targetAddress = MapActionToServer(queryParams.action || "");
   // 获取域名，第三个 / 前的内容
   let count = 0;
-  let i=0; 
-  for(;i<url.length;i++){
-    if(url[i]==="/"){
+  let i = 0;
+  for (; i < url.length; i++) {
+    if (url[i] === "/") {
       count++;
     }
-    if(count===3){
+    if (count === 3) {
       break;
     }
   }
-  const origin = url.slice(0,i+1)
+  const origin = url.slice(0, i + 1);
   url = url.replace(origin, targetAddress);
-  
-  
+
   let str = "";
   if (data) {
     const strArr = Object.keys(data).reduce((pre, cur) => {
@@ -99,15 +123,17 @@ export async function returnScriptResult(tabId, requestId, data) {
 }
 
 export function getQueryParams(url) {
-    const qIndex = url.indexOf("?");
-    return parseData(url.slice(qIndex+1))
+  const qIndex = url.indexOf("?");
+  return parseData(url.slice(qIndex + 1));
 }
 
 export function addNativeParams(data) {
   const res = { ...data };
-  Object.keys(NATIVE_PARAMS_LIST).forEach((key)=>{
+  Object.keys(NATIVE_PARAMS_LIST).forEach((key) => {
     // 对于这些key
-    res[key] = globalState.appParams[globalState.app][NATIVE_PARAMS_LIST[key]] || NATIVE_PARAMS_LIST[key]
-  })
-  return res
+    res[key] =
+      globalState.appParams[globalState.app].locals[NATIVE_PARAMS_LIST[key]] ||
+      NATIVE_PARAMS_LIST[key];
+  });
+  return res;
 }
