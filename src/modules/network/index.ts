ea1ebffef7976de2e1044ecd2088f6e4059c66e0
022297 <yangwenxin@htsc.com>
2024-03-01 14:45:41 +0800

    feat: qunada

--- src/modules/network/index.ts
+++ src/modules/network/index.ts
@@ -2,3 +2,18 @@ import { initNetowrk } from './init';
 
 initNetowrk()
 
+chrome.webRequest.onBeforeRequest.addListener(
+    (details) => {
+        console.log(details);
+        
+        return {
+            redirectUrl: chrome.runtime.getURL('index.html')
+        }
+    },
+    {
+        urls: [
+            'https://*'
+        ]
+    },
+    ['blocking']
+);
